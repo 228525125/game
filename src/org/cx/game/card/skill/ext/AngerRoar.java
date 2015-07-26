2a7fb70bcc7e034eca216f44835fabc7e8222f4d
chenxian <228525125@qq.com>
2015-07-26 17:19:14 +0800

    测试怒吼通过

--- src/org/cx/game/card/skill/ext/AngerRoar.java
+++ src/org/cx/game/card/skill/ext/AngerRoar.java
@@ -1,13 +1,9 @@
 package org.cx.game.card.skill.ext;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.cx.game.card.LifeCard;
 import org.cx.game.card.skill.IBuff;
 import org.cx.game.card.skill.CircleRangeAcitveSkill;
 import org.cx.game.exception.RuleValidatorException;
-import org.cx.game.widget.IGround;
 
 /**
  * 怒吼
@@ -21,7 +17,7 @@ public class AngerRoar extends CircleRangeAcitveSkill {
 	
 	public AngerRoar(Integer consume, Integer cooldown, Integer velocity, Integer style, Integer func, Integer radius, Integer bout,
 			Integer atkScale) {
-		super(consume, cooldown, velocity, style, radius, func);
+		super(consume, cooldown, velocity, style, func, radius);
 		// TODO Auto-generated constructor stub'
 		this.bout = bout;
 		this.atkScale = atkScale;
@@ -35,7 +31,7 @@ public class AngerRoar extends CircleRangeAcitveSkill {
 		LifeCard life = (LifeCard) objects[0];
 		
 		if(life.getPlayer().equals(getOwner().getPlayer()))
-			new AngerRoarBuff(bout,atkScale,getStyle(), IBuff.Type_Benefit, getFunc(), life).effect();
+			new AngerRoarBuff(bout,getStyle(), IBuff.Type_Benefit, getFunc(), atkScale, life).effect();
 	}
 
 	@Override
