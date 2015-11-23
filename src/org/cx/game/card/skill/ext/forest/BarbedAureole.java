a3508873c829e9e7daae77802eb74734c2ea71ff
chenxian <228525125@qq.com>
2015-11-23 16:21:17 +0800

    增加荆棘光环，未测试

---
+++ src/org/cx/game/card/skill/ext/forest/BarbedAureole.java
@@ -0,0 +1,45 @@
+package org.cx.game.card.skill.ext.forest;
+
+import org.cx.game.card.LifeCard;
+import org.cx.game.card.skill.Aureole;
+import org.cx.game.card.skill.IBuff;
+
+public class BarbedAureole extends Aureole {
+
+	private Integer returnRatio = 0;
+	
+	public BarbedAureole(Integer style, Integer range, Integer returnRatio) {
+		super(style, range);
+		// TODO Auto-generated constructor stub
+		this.returnRatio = returnRatio;
+	}
+
+	@Override
+	public void leave(LifeCard life) {
+		// TODO Auto-generated method stub
+		for(IBuff buff : life.getBuffList()){
+			if (buff instanceof BarbedAureoleBuff) {
+				buff.invalid();
+			}
+		}
+	}
+	
+	private static final Integer MaxBout = 999;
+	
+	@Override
+	public void affect(Object... objects) {
+		// TODO Auto-generated method stub
+		super.affect(objects);
+		
+		LifeCard life = (LifeCard) objects[0];
+		
+		new BarbedAureoleBuff(MaxBout, getStyle(), IBuff.Type_Benefit, getFunc(), returnRatio, life).effect();
+	}
+
+	@Override
+	public Class getBuffClass() {
+		// TODO Auto-generated method stub
+		return BarbedAureole.class;
+	}
+
+}
---
+++ src/org/cx/game/card/skill/ext/forest/BarbedAureoleBuff.java
@@ -0,0 +1,58 @@
+package org.cx.game.card.skill.ext.forest;
+
+import org.cx.game.card.LifeCard;
+import org.cx.game.card.skill.Buff;
+import org.cx.game.intercepter.IIntercepter;
+import org.cx.game.intercepter.Intercepter;
+
+public class BarbedAureoleBuff extends Buff {
+
+	private Integer returnRatio = 0;
+	
+	/**
+	 * 
+	 * @param bout
+	 * @param style
+	 * @param type
+	 * @param func
+	 * @param returnRatio 反伤比 基于100
+	 * @param life
+	 */
+	public BarbedAureoleBuff(Integer bout, Integer style, Integer type,
+			Integer func, Integer returnRatio, LifeCard life) {
+		super(bout, style, type, func, life);
+		// TODO Auto-generated constructor stub
+		this.returnRatio = returnRatio;
+	}
+
+	@Override
+	public void effect() {
+		// TODO Auto-generated method stub
+		IIntercepter attackedIn =  new Intercepter(){
+			
+			@Override
+			public void after(Object[] args) {
+				// TODO Auto-generated method stub
+				LifeCard attack = (LifeCard) ((Object[]) args[0])[0];
+				
+				affect(attack);
+			}
+		};
+		
+		recordIntercepter(getOwner().getAttacked(), attackedIn);
+		super.effect();
+	}
+	
+	@Override
+	public void affect(Object... objects) {
+		// TODO Auto-generated method stub
+		LifeCard attack = (LifeCard) objects[0];
+		
+		Integer atk = attack.getAttack().getAtk();
+		Integer damage = atk*returnRatio/100;
+		attack.getDeath().magicToHp(-damage);
+		
+		super.affect(objects);
+		
+	}
+}
